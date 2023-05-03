a442ce968e085d6211b9622aae617730857fa2da
Min Kim <msk@dolbo.net>
2023-05-03 12:01:38 -0700

    Document status module

--- src/status.rs
+++ src/status.rs
@@ -9,17 +9,32 @@ pub struct Status {
 }
 
 impl Database {
+    /// Adds a new status to the database.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if an underlying database operation fails.
     pub async fn add_status(&self, description: &str) -> Result<i32, Error> {
         let conn = self.pool.get().await?;
         conn.insert_into("status", &[("description", Type::TEXT)], &[&description])
             .await
     }
 
+    /// Counts the number of statuses in the database.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if an underlying database operation fails.
     pub async fn count_statuses(&self) -> Result<i64, Error> {
         let conn = self.pool.get().await?;
         conn.count("status", &[], &[], &[]).await
     }
 
+    /// Returns the status with the given id.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if an underlying database operation fails.
     pub async fn load_status(&self, id: i32) -> Result<Status, Error> {
         let conn = self.pool.get().await?;
         conn.select_one_from::<Status>(
@@ -31,6 +46,11 @@ impl Database {
         .await
     }
 
+    /// Returns a list of statuses between `after` and `before`.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if an underlying database operation fails.
     pub async fn load_statuses(
         &self,
         after: &Option<(i32, String)>,
@@ -63,6 +83,11 @@ impl Database {
         .await
     }
 
+    /// Updates the status with the given id.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if an underlying database operation fails.
     pub async fn update_status(&self, id: i32, description: &str) -> Result<(), Error> {
         let conn = self.pool.get().await?;
         conn.update(
