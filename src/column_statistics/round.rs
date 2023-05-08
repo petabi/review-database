7d0dbfc6b9dee91cd086ef2a5a2e001d033613c2
Atsushi Yoshioka <yoshioka234@outlook.com>
2023-05-08 15:29:27 -0500

    Use diesel's query builder instead of select_slice in round

--- src/column_statistics/round.rs
+++ src/column_statistics/round.rs
@@ -1,22 +1,25 @@
-use crate::{tokio_postgres::types::ToSql, Database, Error, OrderDirection, Type};
-use chrono::{naive::serde::ts_nanoseconds, NaiveDateTime};
+use crate::{
+    schema::{cluster::dsl as cl_dsl, event_range::dsl},
+    Database, Error, Type,
+};
+use chrono::NaiveDateTime;
+use diesel::{BoolExpressionMethods, ExpressionMethods, QueryDsl};
+use diesel_async::RunQueryDsl;
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Queryable)]
 #[allow(clippy::module_name_repetitions)]
 pub struct RoundByCluster {
     pub id: i32,
-    #[serde(with = "ts_nanoseconds")]
     pub time: NaiveDateTime,
     pub first_event_id: i64,
     pub last_event_id: i64,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Queryable)]
 #[allow(clippy::module_name_repetitions)]
 pub struct RoundByModel {
     pub id: i32,
-    #[serde(with = "ts_nanoseconds")]
     pub time: NaiveDateTime,
 }
 
@@ -70,34 +73,48 @@ impl Database {
     pub async fn load_rounds_by_cluster(
         &self,
         cluster_id: i32,
-        after: &Option<(i32, i64)>,
-        before: &Option<(i32, i64)>,
+        after: &Option<(i32, NaiveDateTime)>,
+        before: &Option<(i32, NaiveDateTime)>,
         is_first: bool,
         limit: usize,
     ) -> Result<Vec<RoundByCluster>, Error> {
-        let conn = self.pool.get().await?;
-        let mut params: Vec<&(dyn ToSql + Sync)> = vec![&cluster_id];
-        if let Some(cursor) = after {
-            params.push(&cursor.1);
-            params.push(&cursor.0);
+        let limit = i64::try_from(limit).map_err(|_| Error::InvalidInput("limit".into()))? + 1;
+        let mut query = dsl::event_range
+            .select((dsl::id, dsl::time, dsl::first_event_id, dsl::last_event_id))
+            .filter(dsl::cluster_id.eq(&cluster_id))
+            .limit(limit)
+            .into_boxed();
+
+        if let Some(after) = after {
+            query = query.filter(
+                dsl::time
+                    .eq(after.1)
+                    .and(dsl::id.gt(after.0))
+                    .or(dsl::time.gt(after.1)),
+            );
         }
-        if let Some(cursor) = before {
-            params.push(&cursor.1);
-            params.push(&cursor.0);
+        if let Some(before) = before {
+            query = query.filter(
+                dsl::time
+                    .eq(before.1)
+                    .and(dsl::id.lt(before.0))
+                    .or(dsl::time.lt(before.1)),
+            );
         }
-        conn.select_slice(
-            "event_range",
-            &["id", "time", "first_event_id", "last_event_id"],
-            &[("cluster_id", Type::INT4)],
-            &[],
-            &params,
-            &("event_range.time", Type::TIMESTAMP),
-            OrderDirection::Asc,
-            (after.is_some(), before.is_some()),
-            is_first,
-            limit,
-        )
-        .await
+        if is_first {
+            query = query.order_by(dsl::time.asc()).then_order_by(dsl::id.asc());
+        } else {
+            query = query
+                .order_by(dsl::time.desc())
+                .then_order_by(dsl::id.desc());
+        }
+
+        let mut conn = self.pool.get_diesel_conn().await?;
+        let mut rows: Vec<RoundByCluster> = query.get_results(&mut conn).await?;
+        if !is_first {
+            rows = rows.into_iter().rev().collect();
+        }
+        Ok(rows)
     }
 
     /// Returns the rounds in the given model.
@@ -113,36 +130,47 @@ impl Database {
         is_first: bool,
         limit: usize,
     ) -> Result<Vec<RoundByModel>, Error> {
-        let conn = self.pool.get().await?;
-        let cluster_id: i32 = conn
-            .select_one_from(
-                "cluster",
-                &["id"],
-                &[("model_id", Type::INT4)],
-                &[&model_id],
-            )
+        let mut conn = self.pool.get_diesel_conn().await?;
+        let cluster_id = cl_dsl::cluster
+            .select(cl_dsl::id)
+            .filter(cl_dsl::model_id.eq(&model_id))
+            .first::<i32>(&mut conn)
             .await?;
-        let mut params: Vec<&(dyn ToSql + Sync)> = vec![&cluster_id];
-        if let Some(cursor) = after {
-            params.push(&cursor.1);
-            params.push(&cursor.0);
+        let limit = i64::try_from(limit).map_err(|_| Error::InvalidInput("limit".into()))? + 1;
+        let mut query = dsl::event_range
+            .select((dsl::id, dsl::time))
+            .filter(dsl::cluster_id.eq(&cluster_id))
+            .limit(limit)
+            .into_boxed();
+
+        if let Some(after) = after {
+            query = query.filter(
+                dsl::time
+                    .eq(after.1)
+                    .and(dsl::id.gt(after.0))
+                    .or(dsl::time.gt(after.1)),
+            );
         }
-        if let Some(cursor) = before {
-            params.push(&cursor.1);
-            params.push(&cursor.0);
+        if let Some(before) = before {
+            query = query.filter(
+                dsl::time
+                    .eq(before.1)
+                    .and(dsl::id.lt(before.0))
+                    .or(dsl::time.lt(before.1)),
+            );
         }
-        conn.select_slice(
-            "event_range",
-            &["id", "time"],
-            &[("cluster_id", Type::INT4)],
-            &[],
-            &params,
-            &("event_range.time", Type::TIMESTAMP),
-            OrderDirection::Asc,
-            (after.is_some(), before.is_some()),
-            is_first,
-            limit,
-        )
-        .await
+        if is_first {
+            query = query.order_by(dsl::time.asc()).then_order_by(dsl::id.asc());
+        } else {
+            query = query
+                .order_by(dsl::time.desc())
+                .then_order_by(dsl::id.desc());
+        }
+
+        let mut rows: Vec<RoundByModel> = query.get_results(&mut conn).await?;
+        if !is_first {
+            rows = rows.into_iter().rev().collect();
+        }
+        Ok(rows)
     }
 }
