363696f9c0fbe66e1a9a004a12004b751a4c3603
Min Kim <msk@dolbo.net>
2024-09-06 12:56:07 -0700

    Copies trusted domains to redb
    The public API still uses the old trusted domains in RocksDB. The main
    purpose is to make sure the migration works correctly.

---
+++ src/data.rs
@@ -0,0 +1,37 @@
+//! All the models stored in the database.
+
+use std::sync::LazyLock;
+
+use native_db::Models;
+
+pub(crate) static MODELS: LazyLock<Models> = LazyLock::new(|| models().expect("valid model"));
+
+pub type TrustedDomain = v1::TrustedDomain;
+
+pub(crate) fn models() -> anyhow::Result<Models> {
+    let mut models = Models::new();
+    models.define::<TrustedDomain>()?;
+    Ok(models)
+}
+
+pub(crate) mod v1 {
+    use native_db::{native_db, ToKey};
+    use native_model::{native_model, Model};
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Deserialize, Serialize)]
+    #[native_model(id = 1, version = 1)]
+    #[native_db]
+    pub struct TrustedDomain {
+        #[primary_key]
+        pub name: String,
+        pub remarks: String,
+    }
+}
+
+mod tests {
+    #[test]
+    fn validate_models() {
+        assert!(super::models().is_ok());
+    }
+}
