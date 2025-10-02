0904ca9c5a8b2cdcd7e1c4d70889573b4619ffa2
Min Shao <min.shao1988@gmail.com>
2025-10-02 14:07:15 -0700

    Migrate cluster id in column stats table to store cluster id assigned by Reconverge

--- src/migration/migrate_cluster.rs
+++ src/migration/migrate_cluster.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 
 use crate::Database;
@@ -9,6 +11,7 @@ pub(crate) async fn run(database: &Database, store: &crate::Store) -> Result<()>
         "Migrating Clusters for a total of {} models from PostgreSQL to RocksDb",
         models.len()
     );
+    update_cluster_id_in_column_stats(database, store).await?;
     for &model in &models {
         tracing::info!("Migrating Clusters for model {model}");
         if let Err(e) = migrate_clusters_for_model(database, store, model).await {
@@ -34,6 +37,41 @@ async fn remove_clusters(database: &Database, model: i32) -> Result<()> {
     diesel::delete(dsl::cluster.filter(dsl::model_id.eq(model)))
         .execute(&mut conn)
         .await?;
+    Ok(())
+}
+
+async fn update_cluster_id_in_column_stats(
+    database: &Database,
+    store: &crate::Store,
+) -> Result<()> {
+    use diesel_async::RunQueryDsl;
+
+    use crate::diesel::QueryDsl;
+    use crate::schema::cluster::dsl as c_d;
+    use crate::tables::Iterable;
+    let mut conn = database.pool.get().await?;
+
+    let cluster_ids: HashMap<u32, u32> = c_d::cluster
+        .select((c_d::id, c_d::cluster_id))
+        .load::<(i32, i32)>(&mut conn)
+        .await?
+        .into_iter()
+        .map(|(id, cid)| Ok((u32::try_from(id)?, u32::try_from(cid)?)))
+        .collect::<Result<_, anyhow::Error>>()?;
+    let map = store.column_stats_map();
+    let txn = map.transaction();
+    let iter = map.iter(rocksdb::Direction::Forward, None);
+    for item in iter {
+        let old = item?;
+        let mut new = old.clone();
+        new.cluster_id = cluster_ids
+            .get(&old.cluster_id)
+            .copied()
+            .unwrap_or(old.cluster_id);
+        map.update_with_transaction(&old, &new, &txn)?;
+    }
+    txn.commit()?;
+
     Ok(())
 }
 
