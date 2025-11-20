f156a07344b99426e4038d2ce9eb6cbbdcd3e3cd
Min S. Kim <msk@dolbo.net>
2025-11-20 13:33:29 -0800

    Remove unused dependencies and files

--- src/backends/migrations/2025-09-26-182704_remove_tables/up.sql
+++
@@ -1,5 +0,0 @@
-DROP FUNCTION IF EXISTS attempt_cluster_upsert;
-DROP TABLE IF EXISTS cluster;
-DROP INDEX IF EXISTS time_series_cluster_id;
-DROP TABLE IF EXISTS time_series;
-DROP TABLE IF EXISTS model;
