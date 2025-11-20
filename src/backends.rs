8577341f11533cd962816a4830b50f68c020aeb4
Min S. Kim <msk@dolbo.net>
2025-11-20 13:33:29 -0800

    Remove unused dependencies and files

--- src/backends.rs
+++
@@ -1,9 +0,0 @@
-//! Supported database backends.
-//!
-//! This module chooses, at compile-time, one of the supported backends as the
-//! database.
-
-mod postgres;
-
-pub(super) use backend::ConnectionPool;
-use postgres as backend;
